0cb03f03de7e944f27dfd67d48e07ef3ffece30a
Manyanov Amir <mananovamir@gmail.com>
2022-04-13 17:36:06 +0300

    1 commit
    1 commit

---
+++ movie/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+
+class App extends React.Component {
+
+  state =  {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json')
+}
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading} = this.state
+    return <div> {isLoading ? "Loading..." : "Страница загруженна"}
+    </div>
+
+  }
+}
+
+
+export default App;
