6d88499005134b0f8032bac76615288fd7ddcd5e
Manyanov Amir <mananovamir@gmail.com>
2022-04-13 20:10:59 +0300

    2 commit
    2 commit

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,27 +1,39 @@
 import React from "react";
 import axios from "axios";
+import Movie from "./Movie";
 
 class App extends React.Component {
 
-  state =  {
-    isLoading: true,
-    movies: []
-  }
+    state = {
+        isLoading: true,
+        movies: []
+    }
 
-  getMovies = async () => {
-    const movies = await axios.get('https://yts.mx/api/v2/list_movies.json')
-}
+    getMovies = async () => {
+        const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=rating')
+        console.log(movies)
+        this.setState({movies: movies, isLoading : false})
+    }
 
-  componentDidMount() {
-    this.getMovies();
-  }
+    componentDidMount() {
+        this.getMovies();
+    }
 
-  render() {
-    const {isLoading} = this.state
-    return <div> {isLoading ? "Loading..." : "Страница загруженна"}
-    </div>
+    render() {
+        const {isLoading, movies} = this.state
+        return <div> {isLoading ? "Loading..." : movies.map(movie =>{
+            console.log(movie);
+             return <Movie
+                 key={movie.id}
+                 poster={movie.medium_cover_image}
+                 summary={movie.summary}
+                 year={movie.year}
+                 id={movie.id}
+                 title={movie.title}/>
+        })}
+        </div>
 
-  }
+    }
 }
 
 
